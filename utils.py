c7911011a0a44d1a572ab19bbd3ecba360922bd9
loveQt <shmilydxc@vip.qq.com>
2016-11-16 22:35:18 +0800

    add utils

---
+++ utils.py
@@ -0,0 +1,19 @@
+import requests
+import time
+# import datetime
+
+
+def get_html(url, headers=None, data=None):
+    return requests.post(url, headers=headers, data=data).content
+
+
+def datetime_to_timestamp_in_milliseconds():
+    def current_milli_time(): return int(round(time.time() * 1000))
+    return current_milli_time()
+
+
+def timestamp_to_datetime(d):
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(d))
+
+# print int(round(time.time() * 1000))
+# print datetime_to_timestamp_in_milliseconds(datetime.datetime.now())
