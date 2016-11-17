2a92b0a8b611cfb31974a4d9f670a724b614a516
loveQt <shmilydxc@vip.qq.com>
2016-11-17 16:18:45 +0800

    add functions in utils

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 # -*-coding:utf8-*-
+import re
 import requests
 import time
+from bs4 import BeautifulSoup
 # import datetime
 
 
@@ -18,3 +20,30 @@ def timestamp_to_datetime(d):
 
 # print int(round(time.time() * 1000))
 # print datetime_to_timestamp_in_milliseconds(datetime.datetime.now())
+
+
+def get_cids(aid):
+    cids = []
+    av_url = 'http://www.bilibili.com/video/av'+str(aid)+'/'
+    pattern1 = re.compile(r'/video/av.*/(index_.*).html')
+    pattern2 = re.compile(r'cid=(\d{5,10})&aid=(\d{5,10})&pre')
+    av_content = get_html(av_url)
+    indexes = pattern1.findall(av_content)
+    if not len(indexes):  # not == []
+        cids[0] = pattern2.search(av_content).group(1)
+    else:
+        for index in indexes:
+            cid_url = 'http://www.bilibili.com/video/av'+str(aid)+'/'+index+'.html'
+            cid_content = get_html(cid_url)
+            cid = pattern2.search(cid_content).group(1)
+            cids.append(cid)
+    return cids
+
+
+def parse_danmu(cid):
+    danmu_url = 'http://comment.bilibili.com/' + str(cid) + '.xml'
+    xml = get_html(danmu_url)
+    soup = BeautifulSoup(xml)
+    elems = soup.find_all('d')
+    # use elem['p'] elem.text
+    return elems
